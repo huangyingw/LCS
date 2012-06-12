e85e0c80c2800a7122add0c0f0b163bf15a54841
huangyingw@linux_programming <huangyingw@gmail.com>
2012-06-12 14:34:57 -0500

    n

--- src/LCS.java
+++ src/LCS.java
@@ -1,4 +1,18 @@
-
 public class LCS {
+	private String str1;
+	private String str2;
 
+	public LCS(String str1, String str2) {
+		super();
+		this.str1 = str1;
+		this.str2 = str2;
+	}
+
+	public void BruteForce() {
+
+	}
+
+	public void Memorized() {
+
+	}
 }
--- src/test.java
+++ src/test.java
@@ -1,7 +1,5 @@
 public class test {
 	public static void main(String[] args) {
-		LCS lcs = new LCS();
-		G A A T T C A G T T A
-		G G A T C G A
+		LCS lcs = new LCS("GAATTCAGTTA", "GGATCGA");
 	}
 }
